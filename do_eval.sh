0f810c3c4cf17184c2179a6ce5d2e8287873a6a6
zrx <ruixiangzhao@ruc.edu.cn>
2024-06-11 00:11:05 +0800

    update

---
+++ do_eval.sh
@@ -0,0 +1,4 @@
+python evaluation_copy.py --local_rank=0 \
+    --video_feat_path='data/datasets/msrvtt/FeatureData/msrvtt3k-test/msrvtt/msrvtt-7k_xclip+ts2net-as-teacher_vit32/run1' \
+    --text_feat_path='data/datasets/msrvtt/FeatureData/msrvtt3k-query/msrvtt/msrvtt-7k_xclip+ts2net-as-teacher_vit32/run1' \
+    --gt_file_path='data/datasets/msrvtt/Annotations/msrvtt3k-gt.txt'
--- evaluation.py
+++ evaluation.py
@@ -1,152 +1,77 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
-
 import torch
-import numpy as np
-import random
-import os
+import sys
+from utils.bigfile import BigFile
+from tqdm import tqdm
 from metrics import compute_metrics, tensor_text_to_video_metrics, tensor_video_to_text_sim
-import time
 import argparse
-import yaml
-from modules.tokenization_clip import SimpleTokenizer as ClipTokenizer
-from modules.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
-from modules.modeling import CLIP4Clip
-from modules.modeling_xclip import XCLIP
-from modules.modeling_ts2net import TS2Net
-from modules.modeling_xpool import XPool
-from modules.optimization import BertAdam
-
-from util import parallel_apply, get_logger
-from dataloaders.data_dataloaders import DATALOADER_DICT
-from modules.until_module import CrossEn,MaxMarginRankingLoss
-import torch.nn.functional as F
-import time
-
-
-global logger
 
 def get_args(description='CLIP4Clip Distill on Retrieval Task'):
     parser = argparse.ArgumentParser(description=description)
-
-    parser.add_argument('--config_path', type=str, required=True, help='Path to the config.')
-
-    parser.add_argument("--init_model", default=None, type=str, required=False, help="Initial model.")
-    parser.add_argument('--n_gpu', type=int, default=1, help="Changed in the execute process.")
-
-    parser.add_argument("--cache_dir", default="", type=str,
-                        help="Where do you want to store the pre-trained models downloaded from s3")
-
-    parser.add_argument("--world_size", default=0, type=int, help="distribted training")
-    parser.add_argument("--local_rank", default=0, type=int, help="distribted training")
-    parser.add_argument("--rank", default=0, type=int, help="distribted training")
-
+    parser.add_argument('--local_rank', type=int, default=0, help='gpu id')
+    parser.add_argument('--text_feat_path', type=str, default='', help='text_feat_path')
+    parser.add_argument('--video_feat_path', type=str, default='', help='video_feat_path')
+    parser.add_argument('--gt_file_path', type=str, default='', help='gt_file_path')
     args = parser.parse_args()
-    with open(args.config_path, 'r') as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-        for key, value in data.items():
-            for k, v in value.items():
-                setattr(args, k, v)
-
-    if args.sim_header == "tightTransf":
-        args.loose_type = False
-
     return args
 
-def set_seed_logger(args):
-    global logger
-    # predefining random initial seeds
-    random.seed(args.seed)
-    np.random.seed(args.seed)
+def cal_sim_matrix(args):
+    query_ids = []
+    video_ids = []
+    with open(args.gt_file_path, 'r') as f:
+        for line in f.readlines():
+            query_id, video_id = line.strip().split('\t')
+            if query_id not in query_ids:
+                query_ids.append(query_id)
+            if video_id not in video_ids:
+                video_ids.append(video_id)
 
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir, exist_ok=True)
+    video_file = BigFile(args.video_feat_path)
+    text_file = BigFile(args.text_feat_path)
 
-    logger = get_logger(os.path.join(args.output_dir, "eval_results.txt"))
-    return args
+    device = torch.device('cuda:{}'.format(args.local_rank))
+    text_feats = []
+    for query_id in query_ids:
+        text_feat = text_file.read_one(query_id)
+        text_feats.append(text_feat)
+    text_feats = torch.tensor(text_feats).to(device)
+    text_feats = text_feats / text_feats.norm(dim=-1, keepdim=True)
 
-def evaluation(args, test_dataloader):
-    # #################################################################
-    ## below variables are used to multi-sentences retrieval
-    # multi_sentence_: important tag for eval
-    # cut_off_points: used to tag the label when calculate the metric
-    # sentence_num: used to cut the sentence representation
-    # video_num: used to cut the video representation
-    # #################################################################
-    multi_sentence_ = False
-    cut_off_points_, sentence_num_, video_num_ = [], -1, -1
-    if hasattr(test_dataloader.dataset, 'multi_sentence_per_video') \
-            and test_dataloader.dataset.multi_sentence_per_video:
-        multi_sentence_ = True
-        cut_off_points_ = test_dataloader.dataset.cut_off_points
-        sentence_num_ = test_dataloader.dataset.sentence_num
-        video_num_ = test_dataloader.dataset.video_num
-        cut_off_points_ = [itm - 1 for itm in cut_off_points_]
+    video_feats = []
+    for video_id in video_ids:
+        video_feat = video_file.read_one(video_id)
+        video_feats.append(video_feat)
+    video_feats = torch.tensor(video_feats).to(device)
+    video_feats = video_feats / video_feats.norm(dim=-1, keepdim=True)
 
-    if multi_sentence_:
-        logger.warning("Eval under the multi-sentence per video clip setting.")
-        logger.warning("sentence num: {}, video num: {}".format(sentence_num_, video_num_))
+    sim_matrix = torch.einsum('md,nd->mn', text_feats, video_feats)
+    sim_matrix_npy = sim_matrix.cpu().numpy()
+    return sim_matrix_npy
 
-    sim_matrix = np.load(os.path.join(args.output_dir,"sim_matrix.npy"))
-
-    if multi_sentence_:
-        logger.info("before reshape, sim matrix size: {} x {}".format(sim_matrix.shape[0], sim_matrix.shape[1]))
-        cut_off_points2len_ = [itm + 1 for itm in cut_off_points_]
-        max_length = max([e_-s_ for s_, e_ in zip([0]+cut_off_points2len_[:-1], cut_off_points2len_)])
-        sim_matrix_new = []
-        for s_, e_ in zip([0] + cut_off_points2len_[:-1], cut_off_points2len_):
-            sim_matrix_new.append(np.concatenate((sim_matrix[s_:e_],
-                                                  np.full((max_length-e_+s_, sim_matrix.shape[1]), -np.inf)), axis=0))
-        sim_matrix = np.stack(tuple(sim_matrix_new), axis=0)
-        logger.info("after reshape, sim matrix size: {} x {} x {}".
-                    format(sim_matrix.shape[0], sim_matrix.shape[1], sim_matrix.shape[2]))
+def get_metrics(sim_matrix):
+    if sim_matrix.shape[0] != sim_matrix.shape[1]:
+        print("Eval under the multi-sentence per video clip setting.")
+        print("sentence num: {}, video num: {}".format(sim_matrix.shape[0], sim_matrix.shape[1]))
+        sim_matrix = sim_matrix.reshape(sim_matrix.shape[1], -1, sim_matrix.shape[1])
 
         tv_metrics = tensor_text_to_video_metrics(sim_matrix)
         vt_metrics = compute_metrics(tensor_video_to_text_sim(sim_matrix))
     else:
-        logger.info("sim matrix size: {}, {}".format(sim_matrix.shape[0], sim_matrix.shape[1]))
         tv_metrics = compute_metrics(sim_matrix)
         vt_metrics = compute_metrics(sim_matrix.T)
-        logger.info('\t Length-T: {}, Length-V:{}'.format(len(sim_matrix), len(sim_matrix[0])))
 
-    logger.info("Text-to-Video:")
-    logger.info('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
+    print("Text-to-Video:")
+    print('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
                 format(tv_metrics['R1'], tv_metrics['R5'], tv_metrics['R10'], tv_metrics['MR'], tv_metrics['MeanR']))
-    logger.info("Video-to-Text:")
-    logger.info('\t>>>  V2T$R@1: {:.1f} - V2T$R@5: {:.1f} - V2T$R@10: {:.1f} - V2T$Median R: {:.1f} - V2T$Mean R: {:.1f}'.
+    print("Video-to-Text:")
+    print('\t>>>  V2T$R@1: {:.1f} - V2T$R@5: {:.1f} - V2T$R@10: {:.1f} - V2T$Median R: {:.1f} - V2T$Mean R: {:.1f}'.
                 format(vt_metrics['R1'], vt_metrics['R5'], vt_metrics['R10'], vt_metrics['MR'], vt_metrics['MeanR']))
-
-    R1 = tv_metrics['R1']
-    return R1
+    
 
 def main():
-    global logger
     args = get_args()
-    args = set_seed_logger(args)
 
-    tokenizer = ClipTokenizer()
-
-    ## ####################################
-    # dataloader loading
-    ## ####################################
-    assert args.data_type in DATALOADER_DICT
-
-    assert DATALOADER_DICT[args.data_type]["test"] is not None 
-    test_dataloader, test_length = DATALOADER_DICT[args.data_type]["test"](args, tokenizer)
-
-    if args.local_rank == 0:
-        logger.info("***** Running test *****")
-        logger.info("  Num examples = %d", test_length)
-        logger.info("  Batch size = %d", args.batch_size_val)
-        logger.info("  Num steps = %d", len(test_dataloader))
-
-    ## ####################################
-    # eval
-    ## ####################################
-    if args.rank == 0:
-        evaluation(args, test_dataloader)
+    sim_matrix = cal_sim_matrix(args)
+    get_metrics(sim_matrix)
 
 if __name__ == "__main__":
     main()
